87a803063dfddd1561aeaacdf5ac95ecddfcde8d
NikolayK <nikolay.kim@codeimmersive.com>
2019-11-18 16:03:06 -0500

    changes hit points

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function getAttackDamage(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+document.querySelector("#player-health").innerText = player.hitPoints;
+document.querySelector("#monster-health").innerText = monster.hitPoints
 
 let currentEnemy = monster;
 
@@ -8,6 +10,3 @@ document.getElementById("fight").addEventListener("click", fight);
 function fight(){
 currentEnemy.hitPoints - getAttackDamage(player.attackMin, player.attackMax);
 } 
-
-
-
