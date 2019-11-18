1d216ae43de337416e48a9946788add212c28bbd
Jesse Gould <jesse.gould@codeimmersives.com>
2019-11-18 15:49:21 -0500

    starts fight function

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ function getAttackDamage(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+document.getElementById("fight").addEventListener("click", fight);
+function fight(){
+    
+} 
 
+let currentEnemy = monster;
 
