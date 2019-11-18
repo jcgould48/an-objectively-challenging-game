128b1bac1f3d8b75692e2655a73578a3ebb30f88
Jesse Gould <jesse.gould@codeimmersives.com>
2019-11-18 15:53:39 -0500

    first attempt at attack formula

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ function getAttackDamage(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+let currentEnemy = monster;
+
 document.getElementById("fight").addEventListener("click", fight);
 function fight(){
-    
+currentEnemy.hitPoints - getAttackDamage(player.attackMin, player.attackMax);
 } 
 
-let currentEnemy = monster;
+
 
