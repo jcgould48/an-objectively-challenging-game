0db0e4de392de5de73488dece6bbde163c6102da
NikolayK <nikolay.kim@codeimmersive.com>
2019-11-19 10:37:35 -0500

    adds boss and some interface fixes

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 function getAttackDamage(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-document.querySelector("#player-health").innerText = player.hitPoints;
-document.querySelector("#monster-health").innerText = monster.hitPoints
-
 let currentEnemy = monster;
 
-document.getElementById("fight").addEventListener("click", fight);
+update =()=>{
+    document.querySelector("#player-health").innerText = player.hitPoints;
+    document.querySelector("#monster-health").innerText = currentEnemy.hitPoints
+}
+update()
+
+document.querySelector("#fight").addEventListener("click", fight);
+
 function fight(){
-currentEnemy.hitPoints - getAttackDamage(player.attackMin, player.attackMax);
+    if(currentEnemy.hitPoints > 0 && player.hitPoints > 0){
+    currentEnemy.hitPoints -= getAttackDamage(player.attackMin, player.attackMax);
+    player.hitPoints -= getAttackDamage(currentEnemy.attackMin, currentEnemy.attackMax);
+    update()
+    }
+    if(currentEnemy.hitPoints <= 0){
+    document.querySelector("#monster>img").src = "./boss-monster.jpg"
+    currentEnemy = boss;
+    }
+    if(player.hitPoints <= 0){
+        document.querySelector('#results').innerText = 'Mario is dead'
+        document.querySelector('#fight').innerText = 'Reset'
+        document.querySelector('#fight').removeEventListener("click", fight)
+    }
 } 
